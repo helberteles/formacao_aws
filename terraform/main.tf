0a7a723426a90c55186e5528d649757070a9b849
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2024-06-29 21:13:50 -0300

    Upload code

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,20 @@ terraform {
     required_version = ">= 1.2.0"
 }
 
-
 provider "aws" {
     region  = "us-east-1"
     profile = "user-formacao-aws"// configure iam profile
+}
+
+resource "aws_instance" "t3micro-formacao-aws" {
+    ami = "ami-02f3f602d23f1659d" //definida a imagem a ser utilizada
+    instance_type = "t3.micro"
+    tags = {
+    ambiente = "dev"
+    Name = "tag-formacao-aws-dev"
+}
+vpc_security_group_ids = ["sg-018ffb83d58ea0311"]
+    root_block_device {
+        volume_size = 10 //volume do disco
+    }
 }
